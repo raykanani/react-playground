6e8aed15290f2266c5056767693104570d179aae
Ray KananI <hello@raykanani.com>
2020-05-03 22:53:43 -0400

    ignore next dist folder

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ca249aaeef5d5b051f14";
+/******/ 	var hotCurrentHash = "03a34d7bc06e8b3a0a01";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
