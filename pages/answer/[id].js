b9ab864f0e34e74991cc840f4d7880b6df64c29b
Ray KananI <hello@raykanani.com>
2020-05-18 16:47:13 -0400

    add stories

--- pages/answer/[id].js
+++ pages/answer/[id].js
@@ -5,7 +5,7 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Date from '../../components/date'
 import QuestionCard from '../../components/questionCard'
 import Head from 'next/head'
-import ReactPlayer from 'react-player'
+import VideoRecorder from 'react-video-recorder'
 import { getSortedPostsData } from '../../lib/posts'
 import Router from 'next/router'
 
@@ -14,33 +14,6 @@ import utilStyles from '../../styles/utils.module.css'
 
 
 export default function Post({ postData, prevQuestion, nextQuestion }) {
-
-  const [playing, setPlaying] = useState(true);
-  const [answerDuration, setAnswerDuration] = useState(0)
-  const [answerProgressPercent, setAnswerProgressPercent] = useState(0)
-
-  
-
-  const handleDuration = (duration) => {
-    if (postData.answerDuration) {
-      setAnswerDuration(postData.answerDuration)
-    } else {
-      setAnswerDuration(duration)
-    }
-  }
-
-  const handleProgress = (progress) => {
-    answerDuration < progress.playedSeconds && setPlaying(false);
-    // update Percent Progress
-    updateAnswerProgressPercent(answerDuration, progress.playedSeconds);
-    // go to next video if progress past duration
-    answerProgressPercent > 100 && postData.continue && Router.push(`/posts${postData.continue}`)
-  }
-
-  const updateAnswerProgressPercent = (answerLength, answerProgress) => {
-    const currentAnswerProgressPercent = Math.floor((answerProgress / answerLength) * 100);
-    setAnswerProgressPercent(currentAnswerProgressPercent);
-  }
 
   return (
     <Layout prevQuestion={prevQuestion} nextQuestion={nextQuestion}>
@@ -51,25 +24,13 @@ export default function Post({ postData, prevQuestion, nextQuestion }) {
       </Head>
       <article>
       <div className={utilStyles.videoContainer}>
-          {postData.videoUrl && 
-            <ReactPlayer 
-              url={postData.videoUrl}
-              height='100%'
-              width='100%'
-              className={utilStyles.videoPlayer}
-              playing={playing}
-              config={{
-                youtube: {
-                  playerVars: { modestbranding: 1 }
-                }
-              }}
-              onProgress={handleProgress}
-              onDuration={handleDuration}
-            />
-          }
+          <VideoRecorder
+            isOnInitially
+            showReplayControls
+          />
           <div className={utilStyles.overlay}>
             <div className={utilStyles.questionHeader}>
-              <QuestionCard question={postData.title} date={<Date dateString={postData.date} />} answerProgress={answerProgressPercent}/>
+              <QuestionCard question={postData.title} date={<Date dateString={postData.date} />} />
             </div>
           </div>
         </div>
---
+++ posts/crazy-thing.md
@@ -0,0 +1,5 @@
+---
+title: "What's the craziest thing you've done?"
+date: '2020-05-17'
+videoUrl: 'https://www.youtube.com/watch?v=Hjj649ywn18'
+---
